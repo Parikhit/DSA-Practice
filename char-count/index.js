176bec7e8c9a032c0ce1a7b09855a00bcaa9973c
Parikhit <parikhitritzz@gmail.com>
2024-03-01 23:42:19 +0530

    Adding alternate solution

--- char-count/index.js
+++ char-count/index.js
@@ -24,3 +24,32 @@ const charCount = (str) => {
 };
 
 charCount('Parikhit');
+
+// Alternate Solution (efficient than regex)
+
+//Fn to check for alphanumeric for each char
+const isAlphaNumeric = (char) => {
+    let code = char.charCodeAt(0);
+    if (
+        !(code > 47 && code < 58) && //numeric 0-9
+        !(code > 64 && code < 91) && // A-Z
+        !(code > 96 && code < 123) // a-z
+    ) {
+        return false;
+    }
+    return true;
+};
+
+const charCount2 = (str) => {
+    //Create an empty object to return
+    let result = {};
+
+    for (let char of str) {
+        char = char.toLowerCase();
+        if (isAlphaNumeric(char)) {
+            result[char] = ++result[char] || 1;
+        }
+    }
+
+    return result;
+};
