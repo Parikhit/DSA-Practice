11c86b60143024a82d0da9fab2d9d9ae85f88f3c
Parikhit <parikhitritzz@gmail.com>
2024-05-11 23:33:01 +0530

    Sliding window pattern

---
+++ Sliding Window Pattern/general-solution.js
@@ -0,0 +1,28 @@
+/* Write a function called maxSubArraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in an array.
+
+maxSubArraySum([1,2,5,2,8,1,5],2)  //10
+maxSubArraySum([1,2,5,2,8,1,5],4)  //17
+maxSubArraySum([4,2,1,6],1)        //6
+maxSubArraySum([4,2,1,6,2],4)      //13
+maxSubArraySum([],4)               //null
+ */
+
+const maxSubArraySum = (arr, num) => {
+    if (num > arr.length) return null;
+
+    let max = -Infinity;
+
+    for (let i = 0; i < arr.length - num + 1; i++) {
+        let temp = 0;
+
+        for (let j = 0; j < num; j++) {
+            temp += arr[i + j];
+        }
+
+        if (temp > max) max = temp;
+    }
+
+    return max;
+};
+
+maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2);
